44bcd4b67023c78f85f3fe9d6989900ca9e95d9b
fededige <36859839+fededige@users.noreply.github.com>
2022-12-28 12:33:32 +0100

    minor changes

--- src/gui.py
+++ src/gui.py
@@ -48,33 +48,3 @@ button.place(x=370, y=90)
 
 # command=lambda: onClick(inizioInput.get("1.0", "end-1c"), fineInput.get("1.0", "end-1c"))
 root.mainloop()
-
-# window = Tk()
-#
-# window.title("Cerca il cammino")
-# window.geometry('600x450')
-#
-# lbl = Label(window, text="Parola inizio", padx=15)
-# lbl.grid(column=4, row=3)
-# parolaIniz = Entry(window, width=10)
-# parolaIniz.grid(column=4, row=5)
-#
-# lbl2 = Label(window, text="Parola fine", padx=5)
-# lbl2.grid(column=8, row=3)
-# parolaFin = Entry(window, width=10)
-# parolaFin.grid(column=8, row=5)
-#
-# risultato = scrolledtext.ScrolledText(window, width=50, height=22)
-# risultato.grid(column=5, row=15)
-# risultato.insert(INSERT, " ")
-#
-#
-# def clicca():
-#     resul = parolaIniz.get() + " " + parolaFin.get()
-#     risultato.insert(INSERT, " " + calcola(parolaIniz.get(), parolaFin.get()))
-#
-#
-# btn = Button(window, text="Cerca il cammino", command=clicca)
-# btn.grid(column=5, row=9)
-#
-# window.mainloop()
